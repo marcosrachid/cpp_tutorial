dc95498d9cb802baa6fcfaf24e6bcc7663de4951
Marcos Rachid <marcosrachid@gmail.com>
2018-11-24 12:49:49 -0200

    init

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int age;
+    cout << "Digite o ano de nascimento: " << endl;
+    cin >> age;
+    return 0;
+}
