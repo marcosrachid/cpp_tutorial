943cf6a12cd9466190e834ee6d7f5c17b5ec1782
Marcos Rachid <marcosrachid@gmail.com>
2018-12-10 18:13:28 -0200

    debug

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int age;
-    cout << "Digite o ano de nascimento: " << endl;
-    cin >> age;
+    int ano, idade;
+
+    cout << "Digite o ano de nascimento: ";
+    cin >> ano;
+
+    idade = 2018 - ano;
+    for (int i = 0; i < 10; i++) {
+        cout << "Idade: " << idade << endl;
+    }
     return 0;
 }
